f91dc333c58e412edcddf895f0fb0c3b66250fcb
kafka <kafka0238@gmail.com>
2019-11-24 18:03:16 +0300

    add component header and includes components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/header/logo/logo.component';
+import { UserComponent } from './components/header/user/user.component';
+import { InfoComponent } from './components/header/user/info/info.component';
+import { LoginComponent } from './components/header/user/login/login.component';
+import { LogoffComponent } from './components/header/user/logoff/logoff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    UserComponent,
+    InfoComponent,
+    LoginComponent,
+    LogoffComponent
   ],
   imports: [
     BrowserModule
