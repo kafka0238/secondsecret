dc61125ccf5673a6077a22f9d79413280ad39c3b
kafka <kafka0238@gmail.com>
2019-11-24 23:26:15 +0300

    add component page courses and includes search component section with add course and button components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SectionComponent } from './components/courses-page/section/section.component';
+import { SearchComponent } from './components/courses-page/section/search/search.component';
+import { AddCourseButtonComponent } from './components/courses-page/section/add-course-button/add-course-button.component';
+import { CoursesPageComponent } from './components/courses-page/courses-page.component';
+import { ListOfCoursesComponent } from './components/courses-page/list-of-courses/list-of-courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SectionComponent,
+    SearchComponent,
+    AddCourseButtonComponent,
+    CoursesPageComponent,
+    ListOfCoursesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/courses-page/list-of-courses/list-of-courses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-of-courses',
+  templateUrl: './list-of-courses.component.html',
+  styleUrls: ['./list-of-courses.component.sass']
+})
+export class ListOfCoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
