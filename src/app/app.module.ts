8069e8df34b8e08b85e50a69f6f7fa8f9f5089a2
kafka <kafka0238@gmail.com>
2019-11-26 23:29:03 +0300

    add component button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { SectionComponent } from './components/courses-page/section/section.comp
 import { SearchComponent } from './components/courses-page/section/search/search.component';
 import { AddCourseButtonComponent } from './components/courses-page/section/add-course-button/add-course-button.component';
 import { CoursesPageComponent } from './components/courses-page/courses-page.component';
-import { ListOfCoursesComponent } from './components/courses-page/list-of-courses/list-of-courses.component';
+import { CoursesListComponent } from './components/courses-page/courses-list/courses-list.component';
+import { CoursesElementComponent } from './components/courses-page/courses-list/courses-element/courses-element.component';
+import { ButtonComponent } from './components/util/button/button.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { ListOfCoursesComponent } from './components/courses-page/list-of-course
     SearchComponent,
     AddCourseButtonComponent,
     CoursesPageComponent,
-    ListOfCoursesComponent
+    CoursesListComponent,
+    CoursesElementComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
