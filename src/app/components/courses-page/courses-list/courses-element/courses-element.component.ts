0784fdd0716c5c1e5c3892bfbb016ebed039a930
kafka <kafka0238@gmail.com>
2019-11-27 23:53:17 +0300

    add component courses list and element

---
+++ src/app/components/courses-page/courses-list/courses-element/courses-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-element',
+  templateUrl: './courses-element.component.html',
+  styleUrls: ['./courses-element.component.scss']
+})
+export class CoursesElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
