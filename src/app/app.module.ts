4154e923101e4c800df0d8ff45382b78b6c696c3
kafka <kafka0238@gmail.com>
2019-12-10 22:51:33 +0300

    delete useless add course button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { LogoffComponent } from './components/header/user/logoff/logoff.componen
 import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
 import { SectionComponent } from './components/courses-page/section/section.component';
 import { SearchComponent } from './components/courses-page/section/search/search.component';
-import { AddCourseButtonComponent } from './components/courses-page/section/add-course-button/add-course-button.component';
 import { CoursesPageComponent } from './components/courses-page/courses-page.component';
 import { CoursesListComponent } from './components/courses-page/courses-list/courses-list.component';
 import { CoursesElementComponent } from './components/courses-page/courses-list/courses-element/courses-element.component';
@@ -32,7 +31,6 @@ import { FormsModule } from '@angular/forms';
     BreadcrumbsComponent,
     SectionComponent,
     SearchComponent,
-    AddCourseButtonComponent,
     CoursesPageComponent,
     CoursesListComponent,
     CoursesElementComponent,
