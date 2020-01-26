57b3cf02246a445c0b129ffa285618cc890cfc79
kafka <kafka0238@gmail.com>
2020-01-26 21:35:19 +0300

    generate course page module and component

---
+++ src/app/components/course-page/course-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoursePageComponent } from './course-page.component';
+
+@NgModule({
+  declarations: [CoursePageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoursePageModule { }
