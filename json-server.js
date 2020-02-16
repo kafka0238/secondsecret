acda9e5db23c38c3d8ce9d13e4a8eb5856158a77
kafka <kafka0238@gmail.com>
2020-02-16 20:56:22 +0300

    added dev server file json-server.js and db.json

---
+++ json-server.js
@@ -0,0 +1,10 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middleWares = jsonServer.defaults();
+
+server.use(middleWares);
+server.use(router);
+server.listen(3004, () => {
+  console.log('JSON Server is running')
+});
